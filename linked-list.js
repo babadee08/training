a7dbdfb6c24a0221dd23c16a6c5d7a39da734271
Damilare <damilare.durojaye@gmail.com>
2017-08-14 20:09:19 +0100

    Tested Add to head

--- linked-list.js
+++ linked-list.js
@@ -8,3 +8,21 @@ function Node(value, next, prev) {
     this.next = next;
     this.prev = prev;
 }
+
+LinkedList.prototype.addToHead = function(value) {
+    var newNode = new Node(value, this.head, null);
+
+    if (this.head) {
+        this.head.prev = newNode;
+    } else {
+        this.tail = newNode;
+    }
+    this.head = newNode;
+}
+
+var ll = new LinkedList();
+
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(50);
+console.log(ll);

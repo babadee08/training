e210d35952dd24abb0773f8d24bfa30337153471
Damilare <damilare.durojaye@gmail.com>
2017-08-14 20:20:54 +0100

    Add to tail

--- linked-list.js
+++ linked-list.js
@@ -20,9 +20,28 @@ LinkedList.prototype.addToHead = function(value) {
     this.head = newNode;
 }
 
+LinkedList.prototype.addToTail = function(value) {
+    var newNode = new Node(value, null, this.tail);
+
+    if (this.tail) {
+        this.tail.next = newNode;
+    } else {
+        this.head = newNode;
+    }
+    this.tail = newNode;
+}
+
+/**
+ * The code below is for testing
+ * the above functions
+ */
+
 var ll = new LinkedList();
 
 ll.addToHead(100);
 ll.addToHead(200);
 ll.addToHead(50);
+ll.addToTail(300);
+ll.addToTail(93);
+
 console.log(ll);

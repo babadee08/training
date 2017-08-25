0160dd221be7d2fa0ccfc570370b4ebb67a9ff15
Damilare <damilare.durojaye@gmail.com>
2017-08-25 18:50:03 +0100

    tested bst insert

--- bst.js
+++ bst.js
@@ -22,3 +22,16 @@ BST.prototype.insert = function (value) {
 };
 
 var bst = new BST(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
+
+console.log(bst.right.right);

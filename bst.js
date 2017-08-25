267eeac4cb6d644a4aa3f24c6a53fdc0a620c541
Damilare <damilare.durojaye@gmail.com>
2017-08-25 18:39:43 +0100

    setup an insert method on the BST

--- bst.js
+++ bst.js
@@ -4,4 +4,21 @@ function BST(value) {
     this.right = null;
 }
 
+BST.prototype.insert = function (value) {
+    if (value <= this.value) {
+        if (!this.left) {
+            this.left = new BST(value);
+        } else {
+            this.left.insert(value);
+        }
+    }
+    else if (value > this.value) {
+        if (!this.right) {
+            this.right = new BST(value);
+        } else {
+            this.right.insert(value);
+        }
+    }
+};
+
 var bst = new BST(50);
